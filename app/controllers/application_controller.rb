707af8a97ee9af3c97a9a4165c09e05f6ecb05b0
Jeremie <jeremie.bonal@gmail.com>
2016-02-21 18:47:29 +0100

    Added a safeguard for when users are destroyed while someone is logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+    if session[:user_id] && session[:user_id] not in User.all.map(&:id)
+      session[:user_id] = nil
+    end
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
